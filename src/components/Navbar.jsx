aecb806750d64ade4e9ac9e9e5ea6b126fcf23c1
DieCamDu <dcamposd@ucenfotec.ac.cr>
2025-02-14 14:24:56 -0500

    new resume added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
             Skills
           </a>
           <a
-            href="./Diego_Campos_resume.pdf"
+            href="./DCD-resume.pdf"
             target="_blank"
             className="mr-5 hover:text-white"
           >
